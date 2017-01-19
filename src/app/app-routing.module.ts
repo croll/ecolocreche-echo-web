d877f9f5cd1e4ae4ad37eda2dd6f2ed0ae969ce8
Christophe Beveraggi <beve@croll.fr>
2017-01-19 09:19:54 +0100

    app routing: load guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent } from './user/components/login/login.component';
 import { LostPasswordComponent } from './user/components/lost-password/lost-password.component';
 import { ListComponent } from './user/components/list/list.component';
 import { RouterModule, Routes }  from '@angular/router';
+import { AuthGuard } from './auth-guard.service';
 
 const appRoutes: Routes = [
   { path: '', component: PageHomeComponent },
