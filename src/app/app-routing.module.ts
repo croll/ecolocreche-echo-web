20cfb0b2a66c996927f3b223c0eb0b46ff51c3c3
Nicolas Dimitrijevic <nicolas@croll.fr>
2017-01-24 09:13:51 +0100

    update import to avoid conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule }              from '@angular/core';
 import { PageHomeComponent } from './home.component';
 import { PageNotFoundComponent } from './not-found.component';
-import { LoginComponent } from './user/components/login/login.component';
-import { LostPasswordComponent } from './user/components/lost-password/lost-password.component';
-import { ListComponent } from './user/components/list/list.component';
+import { LoginComponent as UserLoginComponent} from './user/components/login/login.component';
+import { LostPasswordComponent as UserLostPasswordComponent} from './user/components/lost-password/lost-password.component';
+import { ListComponent as UserListComponent} from './user/components/list/list.component';
 import { RouterModule, Routes }  from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', component: PageHomeComponent },
-  { path: 'connexion', component: LoginComponent },
-  { path: 'motdepasse', component: LostPasswordComponent },
-  { path: 'utilisateur/liste', component: ListComponent },
+  { path: 'connexion', component: UserLoginComponent },
+  { path: 'motdepasse', component: UserLostPasswordComponent },
+  { path: 'utilisateur/liste', component: UserListComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 @NgModule({
