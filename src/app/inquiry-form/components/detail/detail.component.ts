f51857af38990d1941ffca84521ddd29eab7e884
Christophe Beveraggi <beve@croll.fr>
2017-01-30 00:18:24 +0100

    Inquiry form choices wip

--- src/app/inquiry-form/components/detail/detail.component.ts
+++ src/app/inquiry-form/components/detail/detail.component.ts
@@ -12,8 +12,11 @@ export class DetailComponent implements OnInit {
 
   private id: number;
   item: InquiryForm;
-  themeList: any[];
-  filteredThemeList: any[];
+  childList: any[];
+  filteredChildList: any[];
+  hideUnselected: boolean;
+  level: number = 0;
+  node: any;
 
   constructor(private router: Router, private route: ActivatedRoute, private restService: RestService) {
     this.id = parseInt(this.route.snapshot.params['id']);
@@ -24,13 +27,39 @@ export class DetailComponent implements OnInit {
     this.restService.get(this.id, 'inquiryforms').subscribe(item => {
       this.item = item;
     });
-    this.restService.getList('hist/nodes').subscribe(items => {
-       this.themeList = items;
+    this.getChilds();
+  }
+
+  getChilds(id?: number) {
+    this.level++;
+    let params = null;
+    if (id) {
+      params = {id_node_parent: id}
+      this.getNode(id);
+    }
+    this.restService.getList('hist/nodes', params).subscribe(items => {
+      this.childList = items;
+      this.filteredChildList = items;
     });
   }
 
+  getNode(id) {
+    this.restService.get(id, 'hist/nodes').subscribe(item => {
+      this.node = item;
+    });
+  }
+
+  goToParent(id) {
+    this.level--;
+    this.getChilds(id);
+  }
+
   filterList(filter) {
-    this.filteredThemeList = filter ? this.themeList.filter(item => item.title.toLocaleLowerCase().indexOf(filter.toLocaleLowerCase()) != -1) : this.themeList;
+    this.filteredChildList = filter ? this.childList.filter(item => item.title.toLocaleLowerCase().indexOf(filter.toLocaleLowerCase()) != -1) : this.childList;
+  }
+
+  select(node) {
+    node.selected = true;
   }
 
 }
