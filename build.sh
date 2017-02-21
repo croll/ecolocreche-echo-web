ba3bfc81282c2940a5972c1174f30b66b667fbc0
Nicolas Dimitrijevic <nicolas@croll.fr>
2017-02-21 18:00:25 +0100

    build use -prod

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./node_modules/\@angular/cli/bin/ng build --output-path ../ecolocreche-echos-server/public_html/ -w
+./node_modules/\@angular/cli/bin/ng build --output-path ../ecolocreche-echos-server/public_html/ -prod
