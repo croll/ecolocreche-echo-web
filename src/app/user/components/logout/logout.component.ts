83a94761954ec4ead00288e5207651ef5cf88815
Nicolas Dimitrijevic <nicolas@croll.fr>
2017-02-28 13:27:20 +0100

    display messages on logout

--- src/app/user/components/logout/logout.component.ts
+++ src/app/user/components/logout/logout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '../../services/rest.service';
 import { Router } from '@angular/router';
+import { MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-login',
@@ -10,17 +11,27 @@ import { Router } from '@angular/router';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor(private restService: RestService, private router: Router) { }
+  constructor(private restService: RestService, private router: Router, private snackBar: MdSnackBar) { }
 
   ngOnInit() {
     this.restService.logout()
     .subscribe(user => {
-      console.log("user: ", user);
-      this.router.navigate(["/connexion"]);
+      if (!user) {
+        this.snackBar.open("Déconnection : ", "déconnecté", {
+              duration: 6000,
+            });
+      } else {
+        this.snackBar.open("Déconnection : ", "ca n'a pas marché !", {
+              duration: 6000,
+            });
+      }
     }, err => {
+      this.snackBar.open("Déconnection : ", "ca n'a pas marché !", {
+            duration: 6000,
+          });
       console.log("err: ", err);
-      this.router.navigate(["/"]);
     })
+    this.router.navigate(["/connexion"]);
   }
 
 }
