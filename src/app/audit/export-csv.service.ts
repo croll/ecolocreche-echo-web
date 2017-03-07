64c426587679f80c9cf0744e1c5e3db480e426a2
Christophe Beveraggi <beve@croll.fr>
2017-03-07 21:29:19 +0100

    Audit comparison CSV export OK

--- src/app/audit/export-csv.service.ts
+++ src/app/audit/export-csv.service.ts
@@ -10,7 +10,7 @@ export class ExportCSVService {
 
   getContent(cachedAudit1Datas, cachedAudit2Datas?) {
 
-    let outp = 'THEME;QUESTION;';
+    let outp = 'THEME;RUBRIQUE;QUESTION;';
     if (cachedAudit2Datas) {
       outp += 'REPONSE AUDIT1;IMPACT AUDIT1;REPONSE AUDIT2;IMPACT AUDIT2;'
     } else {
@@ -18,9 +18,10 @@ export class ExportCSVService {
     }
     outp +=  "COMMENTAIRE\r\n";
     let themeList = (cachedAudit2Datas) ? this.auditTools.merge(cachedAudit1Datas.chartDatas.themes, cachedAudit2Datas.chartDatas.themes) : cachedAudit1Datas.chartDatas.themes;
+    let categoriesList = (cachedAudit2Datas) ? Object.assign(cachedAudit1Datas.chartDatas.categories, cachedAudit2Datas.chartDatas.categories) : cachedAudit1Datas.chartDatas.categories;
     let mergedQuestionList = (cachedAudit2Datas) ? this.auditTools.merge(cachedAudit1Datas.questionList, cachedAudit2Datas.questionList) : cachedAudit1Datas.questionList;
-    console.log(themeList);
-    console.log(mergedQuestionList);
+    // console.log(themeList);
+    // console.log(mergedQuestionList);
     // console.log(cachedAudit1Datas);
     // console.log(cachedAudit2Datas);
 
@@ -30,7 +31,7 @@ export class ExportCSVService {
       mergedQuestionList.forEach(question => {
         let addQuestion = false;
         let tmpOutp = '';
-        tmpOutp += '"'+theme.title.replace(/"/g, '`')+'";"'+question.title.replace(/"/g, '`')+'";';
+        tmpOutp += '"'+theme.title.replace(/"/g, '`')+'";"'+categoriesList[question.id_node_parent].title+'";"'+question.title.replace(/"/g, '`')+'";';
         let answer1 = null, answer2 = null;
         // If the question is of same theme
         if (cachedAudit1Datas.questionList[question.id_node] && cachedAudit1Datas.questionList[question.id_node].id_theme == theme.id_node) {
@@ -93,17 +94,26 @@ export class ExportCSVService {
     } else {
       question.value.forEach(val => {
         if (question.type == 'q_checkbox' || question.type == 'q_radio') {
-          obj.value += '"'+val.title.replace(/"/g, '`')+',"'
+          obj.value += val.title.replace(/"/g, '`')
         } else if (question.type == 'q_percents') {
-          obj.value += val.value+'%,"'
+          obj.value += ''+val.value+'%,';
         }
         if (val.comment) {
-          obj.comment += '"'+val.comment.replace(/"/g, '`')+',"';
+          obj.comment += ''+val.comment.replace(/"/g, '`')+',';
         }
         if (val.impact) {
-          obj.impact += '"'+val.impact.label.replace(/"/g, '`')+',"';
+          obj.impact += ''+val.impact.label.replace(/"/g, '`')+',';
         }
       });
+      if (obj.value) {
+        obj.value = this._formatString(obj.value);
+      }
+      if (obj.comment) {
+        obj.comment = this._formatString(obj.comment);
+      }
+      if (obj.impact) {
+        obj.impact = this._formatString(obj.impact);
+      }
     }
     return obj;
   }
@@ -139,6 +149,14 @@ export class ExportCSVService {
       link.dispatchEvent(event);
       document.body.removeChild(link);
     }
+  }
+
+  private _formatString(str) {
+    str = str.toString().trim();
+    if (str.substr(-1, 1) == ',') {
+       str = str.substr(0, str.length-1);
+     }
+    return '"'+str+'"';
   }
 
 
