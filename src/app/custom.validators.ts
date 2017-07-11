b31dda26e466962ed62ec788275918b77fdb62e0
Christophe Beveraggi <beve@croll.fr>
2017-07-11 11:14:16 +0200

    Added custom validator for date

---
+++ src/app/custom.validators.ts
@@ -0,0 +1,13 @@
+import {FormControl} from '@angular/forms';
+
+export class CustomValidators {
+
+    static frenchDate(control: FormControl): any {
+      let frenchDatePattern = /^\d{1,2}\/\d{1,2}\/\d{4} \d{2}:\d{2}$/;
+
+      if (!control.value.match(frenchDatePattern))
+        return { "frenchDate": true };
+
+        return null;
+    }
+}
