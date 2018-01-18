90537c44881a81b667e61553165c157f36e76921
Christophe Beveraggi <beve@croll.fr>
2018-01-18 16:46:18 +0100

    update inquiry form object

--- src/app/inquiry-form/inquiry-form.ts
+++ src/app/inquiry-form/inquiry-form.ts
@@ -6,6 +6,8 @@ export class InquiryForm {
   public comment: string = ''
   public position: number = 0
   public nodeslist: string;
+  public mail_body: string;
+  public mail_title: string;
 }
 
 export class InquiryFormExt extends InquiryForm {
