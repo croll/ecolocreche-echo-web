c2c74dd86b7c69f39982f821e84106932a436ff1
Nicolas Dimitrijevic <nicolas@croll.fr>
2018-01-30 15:24:24 +0100

    add active audit listing export has xlsx, with auto-load of the xlsx module

--- src/app/audit/components/active-list/active-list.component.ts
+++ src/app/audit/components/active-list/active-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RestService } from '../../../rest.service';
 import { Audit } from '../../audit';
+import { saveAs } from 'file-saver';
 
 @Component({
   templateUrl: './active-list.component.html',
@@ -21,8 +22,95 @@ export class ActiveListComponent implements OnInit {
   ngOnInit() {
   }
 
-  filterList(filter) {
-    this.filteredList = filter ? this.list.filter(item => item.establishment.name.toLocaleLowerCase().indexOf(filter.toLocaleLowerCase()) != -1) : this.list;
+  filterList(filterString, filterCompleted) {
+    console.log("filterCompleted : ", filterCompleted);
+    let filteredList = this.list;
+    if (filterString && filterString.length > 0)
+      filteredList = filteredList.filter(item => item.establishment.name.toLocaleLowerCase().indexOf(filterString.toLocaleLowerCase()) != -1)
+    if (filterCompleted)
+      filteredList = filteredList.filter(item => item.cached_percent_complete < 0.999)
+    this.filteredList = filteredList;
   }
 
+  exportCSV() {
+    import('xlsx').then(XLSX => {
+      let data = [];
+      data.push([
+        "ID",
+        "clef d'audit",
+        "Etablissement",
+        "Type",
+        "Statut",
+        "Code postal",
+        "mail",
+        "date audit",
+        "% fait",
+        "% ignoré",
+      ]);
+      this.filteredList.forEach(function(audit) {
+        //console.log("audit: ", audit);
+        data.push([
+          audit.id,
+          audit.key,
+          audit.establishment.name,
+          audit.establishment.type,
+          audit.establishment.status,
+          audit.establishment.postalcode,
+          audit.establishment.mail,
+          audit.date_start,
+          audit.cached_percent_complete,
+          audit.cached_percent_ignored,
+        ]);
+      });
+
+      /* generate worksheet */
+      const ws = XLSX.utils.aoa_to_sheet(data);
+
+      // convert cellules
+      for (let k in ws) {
+
+        // convert dates
+        if (k!='H1' && k.startsWith('H')) {
+          ws[k].t='d';
+        }
+
+        // convert cached_percent_complete
+        if (k!='I1' && k.startsWith('I')) {
+          ws[k].z='0%';
+        }
+
+        // convert cached_percent_ignored
+        if (k!='J1' && k.startsWith('J')) {
+          ws[k].z='0%';
+        }
+
+      }
+
+      // set col width
+      ws['!cols'] = [
+        {wch:4},
+        {wch:36},
+        {wch:40},
+        {wch:12},
+        {wch:12},
+        {wch:8},
+        {wch:32},
+        {wch:10},
+        {wch:6},
+        {wch:6},
+      ];
+
+
+      console.log("ws: ", ws);
+
+      /* generate workbook and add the worksheet */
+      const wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+      /* save to file */
+      const wbout: string = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+      saveAs(new Blob([wbout]), 'SheetJS.xlsx');
+
+    });
+  }
 }
