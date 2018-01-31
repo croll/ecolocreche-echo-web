a66e95a94210cebbb88a2804ed9e6e13159c516a
Nicolas Dimitrijevic <nicolas@croll.fr>
2018-01-31 11:53:20 +0100

    - add audits filters (inquiry form, and not 100% complete) - resolve review to complete only when really completed

--- src/app/audit/audit-list.resolver.ts
+++ src/app/audit/audit-list.resolver.ts
@@ -5,29 +5,29 @@ import { RestService } from '../rest.service';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
+export class AuditListResolved {
+  audits: any[];
+  plop: number;
+}
+
 @Injectable()
-export class AuditListResolver implements Resolve<any> {
+export class AuditListResolver implements Resolve<AuditListResolved> {
 
   constructor(private restService: RestService, private http: Http) {
   }
 
   resolve(route: ActivatedRouteSnapshot):Observable<any> {
-    return this.restService.getList('audits', {active: 1, sort: '-date_start'})
+    return this.restService.getList('audits', {active: 1, inquiry_type: 'audit', sort: '-date_start'})
       .flatMap(audits => {
-        let done = 0;
-         return Observable.create(observer => {
-          audits.forEach(audit => {
-            done++;
-            this.restService.get(audit.id_inquiryform, 'hist/inquiryforms').subscribe(iq => {
-              audit.inquiryform = iq;
-            })
-            if (audits.length == done) {
-              observer.next(audits);
-              observer.complete();
-            }
-          });
+        let observable_audits: Observable<any>[] = [];
+        audits.forEach(audit => {
+          observable_audits.push(this.restService.get(audit.id_inquiryform, 'hist/inquiryforms', { with_deleteds: 1 }).map(iq => {
+            audit.inquiryform = iq;
+          }));
+        });
+        return Observable.forkJoin(observable_audits, plop => {
+          return audits;
+        });
       });
-  });
-  }
-
+    }
 }
