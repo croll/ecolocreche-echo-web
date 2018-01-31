cb3907b83bdcc4406b4d37bcd251cf10490176cf
Nicolas Dimitrijevic <nicolas@croll.fr>
2018-02-01 00:39:16 +0100

    do not update inquiryform client-side when adding/deleting theme in recap action

--- src/app/inquiry-form-recap-actions/components/edit-theme/edit-theme.component.ts
+++ src/app/inquiry-form-recap-actions/components/edit-theme/edit-theme.component.ts
@@ -51,7 +51,7 @@ export class EditThemeComponent implements OnInit {
   save() {
     this.restService.save(Object.assign(this.item, this.echosForm.value), 'hist/nodes', {id_inquiryform: this.inquiryform.id_inquiryform}, 'id_node').subscribe((RecapActions) => {
       if (!this.item.id_node) {
-        this.updateNodeslist(RecapActions.id_node, 'add');
+        //this.updateNodeslist(RecapActions.id_node, 'add');
       } else {
         this.goBack();
       }
@@ -63,13 +63,14 @@ export class EditThemeComponent implements OnInit {
   delete() {
     if (confirm("Souhaitez vous vraiment supprimer ce thème ?")) {
       this.restService.delete(this.item.id_node, 'hist/nodes', {id_inquiryform: this.inquiryform.id_inquiryform}).subscribe((response) => {
-        this.updateNodeslist(this.item.id_node, 'delete');
+        //this.updateNodeslist(this.item.id_node, 'delete');
       }, (err) => {
         console.error(err);
       });
     }
   }
 
+/*
   updateNodeslist(id, action) {
     if (!id) {
       return false;
@@ -90,7 +91,8 @@ export class EditThemeComponent implements OnInit {
       console.error(err);
     });
   }
-
+*/
+*
   unsetNodeLink() {
     this.linkedToNodeIdCtrl.setValue(null);
   }
