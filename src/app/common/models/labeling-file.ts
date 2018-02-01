77c57fdfd7b43bbc1a4ebe1bb248401ae33c90ee
Christophe Beveraggi <beve@croll.fr>
2018-02-01 11:46:41 +0100

    - labeling file model

---
+++ src/app/common/models/labeling-file.ts
@@ -0,0 +1,8 @@
+export class LabelingFile {
+  public id: number
+  public id_establishment: number
+  public id_labelingfile_src: number
+  public id_audit1: number
+  public id_audit2: number
+  public id_audit_recap_actions: number
+}
