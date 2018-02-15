c7bf491d21be5f9418d2fbda20a5ee3535df3e2b
Nicolas Dimitrijevic <nicolas@croll.fr>
2018-02-15 14:16:41 +0100

    do not list "not audit" node in questionnaires

--- src/app/inquiry-form-audit/inquiry-form-tree.resolver.ts
+++ src/app/inquiry-form-audit/inquiry-form-tree.resolver.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { Node } from '../common/models/node';
 import { RestService } from '../rest.service';
+import { InquiryForm } from '../common/models/inquiry-form';
 
 @Injectable()
 export class InquiryFormTreeResolver implements Resolve<Node[]> {
@@ -9,6 +10,8 @@ export class InquiryFormTreeResolver implements Resolve<Node[]> {
   constructor(private restService: RestService) {}
 
   resolve(route: ActivatedRouteSnapshot):any {
-    return this.restService.getList('hist/nodes?recurse=1', {});
+    return this.restService.getList('hist/nodes?recurse=1', {
+      inqinquiry_type: InquiryForm.Inquiry_type.Audit,
+    });
   }
 }
