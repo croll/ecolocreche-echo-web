fc9404b3f1b84faafa5bc5ad3a14fcb84bafc5b1
Nicolas Dimitrijevic <nicolas@croll.fr>
2018-02-19 11:23:12 +0100

    recap action now have an url with a key, not an id

--- src/app/recap-actions/routing.module.ts
+++ src/app/recap-actions/routing.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
         }
       },
       {
-        path: ':id',
+        path: ':key',
         component: AnswerComponent,
         resolve: {
           infos: AuditResolver
